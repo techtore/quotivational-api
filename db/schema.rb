85bce7d8ba7e5a94ca7bf2457355752772e9f916
Victoria Hale <vic.hale74@gmail.com>
2020-02-10 11:43:22 -0800

    author model and table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_193157) do
+ActiveRecord::Schema.define(version: 2020_02_10_194238) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "quotes", force: :cascade do |t|
     t.text "body"
