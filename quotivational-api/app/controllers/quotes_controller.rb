f3aa53a6e783c05ef16592b93d3c00a3c2ed5954
Victoria Hale <vic.hale74@gmail.com>
2020-02-10 13:46:50 -0800

    edited quote_params

--- quotivational-api/app/controllers/quotes_controller.rb
+++ quotivational-api/app/controllers/quotes_controller.rb
@@ -1,7 +1,7 @@
 class QuotesController < ApplicationController
     def index 
         quotes = Quote.all
-        render json: quotes
+        render json: QuoteSerializer.new(quotes)
     end
 
     def show
