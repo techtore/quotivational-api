77c9063306760ac6cdf4dddf988ca5a317d568fe
Victoria Hale <vic.hale74@gmail.com>
2020-02-10 12:01:33 -0800

    controller actions

--- quotivational-api/app/controllers/authors_controller.rb
+++ quotivational-api/app/controllers/authors_controller.rb
@@ -1,2 +1,23 @@
 class AuthorsController < ApplicationController
+
+    def index 
+        authors = Author.all
+        render json: authors
+    end
+
+    def show
+        author = Author.find(params[:id])
+        render json: author
+    end
+
+    def create
+        author = Author.new(author_params)
+        if author.save 
+            render json: author
+        else 
+            status: 400
+        end
+    end
+
+
 end
--- quotivational-api/app/controllers/quotes_controller.rb
+++ quotivational-api/app/controllers/quotes_controller.rb
@@ -1,2 +1,20 @@
 class QuotesController < ApplicationController
+    def index 
+        quotess = Quote.all
+        render json: quotes
+    end
+
+    def show
+        quote = Quote.find(params[:id])
+        render json: quote
+    end
+
+    def create
+        quote = Quote.new(quote_params)
+        if quote.save 
+            render json: quote
+        else 
+            status: 400
+        end
+    end
 end
