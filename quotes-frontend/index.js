a6627f01a5c4509a6ac8a2468c35eef4bc21c837
Victoria Hale <vic.hale74@gmail.com>
2020-02-11 11:16:21 -0800

    added frontend files

---
+++ quotes-frontend/index.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script type="text/javascript" src="index.js"></script>
+    <title>Quotivational</title>
+  </head>
+  <body>
+    <header>
+      <h2>Quotivational</h2>
+    </header>
+
+    <main>
+      
+    </main>
+
+  </body>
+</html>
