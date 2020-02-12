3d2a4954a4ab4c2e6d89b4ab1cfaaf5d0ffb1e3f
Victoria Hale <vic.hale74@gmail.com>
2020-02-11 21:09:32 -0800

    test.js file to test fetch and render functions, debugging render errors

--- quotes-frontend/index.js
+++ quotes-frontend/index.js
@@ -7,43 +7,83 @@ window.addEventListener('load', () => {
 })
 
 //server requests
-function getQuotes(){
+function getQuotes() {
     return fetch(QUOTES_URL)
-    .then(rep => rep.json())
-    .then(quotes => { 
-            const quotesContainer = document.querySelector('#quotes-container');
-            quotesContainer.innerHTML = '';
-            quotes.forEach(quote => {
-                newQuote = new Quote(quote);
-                newQuote.renderQuote();
-            })
-        })
+    .then(resp => resp.json())
+    .then(data => {
+        const quotesContainer = document.querySelector('.quotes-container');
+        quotesContainer.innerHTML = '';
+        data.forEach(quote => {
+          console.log(quote);
+          let newQuote = new Quote(quote);
+          newQuote.renderQuotes(quote);
+        });
+      });
+        
+    }
+        class Quote {
+            constructor(quoteObj){
+                this.id = quoteObj.id
+                this.body = quoteObj.body
+                this.image = quoteObj.image_url
+                this.author = quoteObj.author
+        
+            }
+        
 
-}
-// function getLocations() {
-//     fetch(BASE_URL + '/locations')
-//       .then(resp => resp.json())
-//       .then(data => {
-//         const locationContainer = document.querySelector('#location-container');
-//         locationContainer.innerHTML = '';
-//         data.forEach(location => {
-//           console.log(location);
-//           let newLocation = new Location(location);
-//           newLocation.renderLocation();
-//         });
-//       });
-//   }
+         renderQuotes(){
+            //intance method 
+            // let quotesContainer = document.querySelector('.quotes-container')
+            // quotesContainer.innerHTML = ""
+            // let quoteCard = document.createElement('div')
+            // quoteCard.dataset.id = this.id
+            // quoteCard.className = 'quote-card'
+            // let quoteTextDiv = document.createElement('div')
+            // quoteTextDiv.className = 'quote-text'
+            // let quoteBodyDiv = document.createElement('div')
+            // let quoteBody = document.createElement('p')
+            // quoteBody.innerHTML = this.body
+            // quoteBodyDiv.append(quoteBody)
+            // let quoteAuthorDiv = document.createElement('div')
+            // let quoteAuthor = document.createElement('h4')
+            // quoteAuthor.innerText = `-${this.author}`
+            // quoteAuthorDiv.append(quoteAuthor)
+            // const deleteBtn = document.createElement('button')
+            // deleteBtn.className = `deletebtn`
+            // deleteBtn.innerText = `Delete`
+            // deleteBtn.dataset.id = this.id
+            // deleteBtn.addEventListener('click', (e) => {
+            //     let deleteResponse = confirm(`Are you sure you want to delete this quote?`)
+            //     if (deleteResponse == true) {
+            //         deletQuoteCard(e)
+            //     }
+            // quoteTextDiv.append(quoteBodyDiv)
+            // quoteTextDiv.append(quoteAuthorDiv)
+            // quoteTextDiv.append(deleteBtn)
+            // quoteCard.append(quoteTextDiv)
+            // const quoteImgDiv = document.createElement('div')
+            // quoteImgDiv.className('quote-img')
+            // const quoteImg = document.createElement('img')
+            // quoteImg.src = this.image
+            // quoteImg.alt = 'Quote Image'
+            // quoteImgDiv.append(quoteImg)
+            // quoteCard.append(quoteImgDiv)
+            // quotesContainer.append(quoteCard)
+            
+        }
+    }
+           
+      
+// function getAuthors(){
+//     return fetch(AUTHORS_URL)
+//     .then(rep => rep.json())
+//     .then(authors => { 
+//         authors.forEach(author => {
+//             console.log(author)
+//         })
 
-function getAuthors(){
-    return fetch(AUTHORS_URL)
-    .then(rep => rep.json())
-    .then(authors => { 
-        authors.forEach(author => {
-            console.log(author)
-        })
+//     })
 
-    })
-}
 
 // function renderAuthors(authors){
 
