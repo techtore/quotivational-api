d1c9fa5b735d461edb4416c16e5aa71e49cbb59d
Victoria Hale <vic.hale74@gmail.com>
2020-02-13 10:50:19 -0800

    view and add buttons working

--- quotivational-api/quotes-backend/config/routes.rb
+++ quotivational-api/quotes-backend/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :quotes 
-  resources :authors
+ 
+  resources :authors 
+  resources :quotes
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
